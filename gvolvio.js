b748c72fcd6caa58847986f3d71ba02ade591a02
nebrelbug <nebrelbug@gmail.com>
2017-05-31 16:37:11 -0600

    Update gvolvio.js

--- gvolvio.js
+++ gvolvio.js
@@ -110,9 +110,9 @@ processing.draw = function() {
 changeRef.on('value', function(snapshot) {
    processing.background(0,0,0);
   snapshot.forEach(function(childSnapshot) {
-    polygon(childSnapshot.val().sides, (childSnapshot.val().xpos-xpos)+(processing.width/2-radius/2), (childSnapshot.val().ypos-ypos)+(processing.width/2-radius/2), childSnapshot.val().radius, childSnapshot.val().fillColor, 0)
+    polygon(childSnapshot.val().sides, (childSnapshot.val().xpos-xpos)+(processing.width/2-radius/2), (childSnapshot.val().ypos-ypos)+(processing.height/2-radius/2), childSnapshot.val().radius, childSnapshot.val().fillColor, 0)
     processing.fill(255,0,0);
-    processing.text(childSnapshot.val().name, (childSnapshot.val().xpos-xpos)+(processing.width/2-radius/2), (childSnapshot.val().ypos-ypos)+(processing.width/2-radius/2));
+    processing.text(childSnapshot.val().name, (childSnapshot.val().xpos-xpos)+(processing.width/2-radius/2), (childSnapshot.val().ypos-ypos)+(processing.height/2-radius/2));
     processing.fill(255);
   });
 });
@@ -153,4 +153,4 @@ $( "#signOut" ).click(function () {
 	
 
 });
-//V 0.8
+//V 0.9
