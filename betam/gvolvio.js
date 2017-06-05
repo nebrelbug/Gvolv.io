312784d387527f211d35c805094ee5aaad2284c9
acetousk <acetousk@gmail.com>
2017-06-04 19:29:37 -0600

    Update gvolvio.js

--- betam/gvolvio.js
+++ betam/gvolvio.js
@@ -14,42 +14,6 @@ var sides = 5;
 var radius = 20;
 var fillColor = 255;
 var strokeColor;
-
-	
-$( "#signIn" ).click(function() {
-
-firebase.auth().signInWithRedirect(provider);
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-});
-	
-	
-	
-	firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-	uid = user.uid;
-	name = user.displayName;
-	userRef = firebase.database().ref('users/'+uid);
-    	function sketchProc(processing) {
-		
-//POLYGON CREATION
 		
 function polygon(sides, centerX, centerY, radius, fillColor, strokeColor) {
     processing.fill(fillColor);
@@ -153,13 +117,6 @@ var processingInstance = new Processing(canvas, sketchProc);
   }
 });
 	
-$( "#signOut" ).click(function () {
-	userRef.remove();
-	firebase.auth().signOut().then(function() {
-  location.reload();
-}).catch(function(error) {
-  // An error happened.
-});
 	
 });
 	
